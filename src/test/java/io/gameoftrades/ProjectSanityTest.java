7a6a0cd2caa5294b76be8cd32493b329141904fd
Erik Hooijmeijer <erik.hooijmeijer@42.nl>
2016-07-14 08:14:06 +0200

    added pom.xml check

--- src/test/java/io/gameoftrades/ProjectSanityTest.java
+++ src/test/java/io/gameoftrades/ProjectSanityTest.java
@@ -1,7 +1,15 @@
 package io.gameoftrades;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Test;
 
@@ -48,6 +56,18 @@ public class ProjectSanityTest {
             fail("HandelaarImpl kon niet geinstantieerd worden met een no-args constructor.");
         } catch (IllegalAccessException e) {
             fail("de no-args constructor van HandelaarImpl is niet public.");
+        }
+    }
+
+    @Test
+    public void heeftPomNieuwArtifactIdGegeven() {
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File("./pom.xml")))) {
+            List<String> aid = reader.lines().filter(s -> s.contains("<artifactId>")).collect(Collectors.toList());
+            for (String s : aid) {
+                assertFalse(s.contains("gameoftrades-student-kit"));
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
         }
     }
 
